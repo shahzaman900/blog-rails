2585cef5367fc22f221ba6eb65c77800dea34df2
sheezu900 <shahzamanlive2@gmail.com>
2023-11-04 20:48:24 +0500

    add model classes and implementstion and also add associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,12 @@
 class Like < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  after_save :update_like_counter
+
+  private
+
+  def update_like_counter
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  after_save :update_post_counter
+
+  def most_recent_comments
+    comments.order(created_at: :desc).includes(:author).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 end
