6458f788c504f077101efdc27d0e92fa18eb5873
sheezu900 <shahzamanlive2@gmail.com>
2023-11-05 01:03:18 +0500

    now fixed

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'John', posts_counter: 30, photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
+  subject { User.new(name: 'John', post_counter: 30, photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
 
   before { subject.save }
 
@@ -10,18 +10,18 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'posts_counter should be present' do
-    subject.posts_counter = nil
+  it 'post_counter should be present' do
+    subject.post_counter = nil
     expect(subject).to_not be_valid
   end
 
-  it 'posts_counter should be integer' do
-    subject.posts_counter = 'a'
+  it 'post_counter should be integer' do
+    subject.post_counter = 'a'
     expect(subject).to_not be_valid
   end
 
-  it 'posts_counter should be greater than or equal to zero' do
-    subject.posts_counter = -1
+  it 'post_counter should be greater than or equal to zero' do
+    subject.post_counter = -1
     expect(subject).to_not be_valid
   end
 end
