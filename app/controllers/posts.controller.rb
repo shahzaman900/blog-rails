89e6ac73e987593e9bf9de53d2e8ddd4a844ada4
sheezu900 <shahzamanlive2@gmail.com>
2023-11-11 01:22:44 +0500

    changes in post controller

--- app/controllers/posts.controller.rb
+++ app/controllers/posts.controller.rb
@@ -13,44 +13,4 @@ class PostsController < ApplicationController
     @post = @user.posts.includes(:comments, :likes).find(params[:id])
   end
 
-  def new
-    @post = Post.new
-    @comment = Comment.new
-    @like = Like.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author = current_user
-    if @post.save
-      redirect_to user_posts_path(current_user)
-    else
-      render :new, locals: { post: @post }
-    end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to user_posts_path(current_user), notice: 'Post was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted.'
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
-  end
 end
