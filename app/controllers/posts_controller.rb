0237a9faf07393de21d1f1b2f07a7b58ad771aea
sheezu900 <shahzamanlive2@gmail.com>
2023-11-11 02:06:00 +0500

    add post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,11 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resourcecls
 
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
 
     render json: @posts, except: %i[created_at updated_at]
-  end
-
-  def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.includes(:comments, :likes).find(params[:id])
   end
 
 end
