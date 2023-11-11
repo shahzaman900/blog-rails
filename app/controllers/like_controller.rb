a9296b32b5c7ef31b840680d2666f381167771be
sheezu900 <shahzamanlive2@gmail.com>
2023-11-12 00:51:11 +0500

    some changes

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -4,10 +4,10 @@ class LikesController < ApplicationController
     @like = Like.new(post_id: params[:post_id], user_id: params[:user_id])
     @like.user = current_user
     if @like.save
-      flash[:success] = 'Liked the Post!'
+      flash[:success] = 'Liked the Post pleas!'
       redirect_to user_post_path(id: @like.post_id, user_id: @like.user_id)
     else
-      flash.now[:error] = 'Like to the Post could not be saved!'
+      flash.now[:error] = 'Like to thee post will not saved!!'
     end
   end
 end
