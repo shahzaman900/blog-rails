1ce2cff9e4fc36e9090b8bfe913e74d7f6c9e2ef
sheezu900 <shahzamanlive2@gmail.com>
2023-11-11 23:36:24 +0500

    add view to in html files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
 
-# user = User.create(
-#   name: 'Shah Zaman',
-#   photo: 'https://picsum.photos/200',
-#   bio: 'dbflaskjdbfasdbfalsdbfasdblf',
-#   post_counter: 10
-# )
+# Create users
+user1 = User.create(name: 'John Doe', photo: 'john.jpg', bio: 'Web Developer', post_counter: 0)
+user2 = User.create(name: 'Jane Smith', photo: 'jane.jpg', bio: 'Designer', post_counter: 0)
 
-# 20.times do |i|
-#   User.create(
-#     name: "Shah Zaman #{i+1}",
-#   photo: 'https://picsum.photos/200',
-#   bio: 'dbflaskjdbfasdbfalsdbfasdblf',
-#   post_counter: 10
-#   )
-# end
+# Create posts for each user
+post1 = user1.posts.create(title: 'First Post by John', text: 'This is the content of the first post by John.')
+post2 = user1.posts.create(title: 'Second Post by John', text: 'Another post by John.')
 
+post3 = user2.posts.create(title: 'First Post by Jane', text: 'This is the content of the first post by Jane.')
 
-# 5.times do |i|
-#   Post.create(
-#     title: "Post #{i+1}",
-#     text: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
-#     comments_counter: 2,
-#   )
-# end
+# Create comments on posts
+Comment.create(text: 'Great post!', user: user2, post: post1)
+Comment.create(text: 'Interesting thoughts.', user: user1, post: post3)
+
+# Create likes on posts
+Like.create(user: user1, post: post2)
+Like.create(user: user2, post: post3)
+
+puts 'Seed data has been created!'
