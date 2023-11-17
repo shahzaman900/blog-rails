71a82d8499a11f271ae38929e689bcddf944929a
sheezu900 <shahzamanlive2@gmail.com>
2023-11-17 14:43:43 +0500

    setup capybara

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
-# class PostsController < ApplicationController
-#   # load_and_authorize_resourcecls
+class PostsController < ApplicationController
+  # load_and_authorize_resourcecls
 
-#   def index
-#     @user = User.find(params[:user_id])
-#     @posts = @user.posts.includes(:comments)
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+  end
 
-#     render json: @posts, except: %i[created_at updated_at]
-#   end
-
-# end
+end
 
 
     def new
